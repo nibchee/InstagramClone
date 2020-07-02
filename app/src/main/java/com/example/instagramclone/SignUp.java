304d5ac0a556d5325c46cca43586896c026eca8d
nibchee <51364680+nibchee@users.noreply.github.com>
2020-07-03 00:08:15 +0530

    Get Data Feom Parse Server

--- app/src/main/java/com/example/instagramclone/SignUp.java
+++ app/src/main/java/com/example/instagramclone/SignUp.java
@@ -5,15 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.parse.Parse;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
-import java.io.IOException;
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,7 +24,11 @@ public class SignUp extends AppCompatActivity implements  View.OnClickListener {
 
     private EditText punch_Power,kick_Speed,kick_Power,punch_Speed,name;
     Button btncreate;
+    private TextView txtGetData;
+    private Button btnGetAllData;
+    private String allKickBoxers;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
@@ -31,9 +38,59 @@ public class SignUp extends AppCompatActivity implements  View.OnClickListener {
          punch_Power=findViewById(R.id.punch_Power);
          name=findViewById(R.id.name);
         btncreate=findViewById(R.id.btnCteate);
+       txtGetData=findViewById(R.id.txtGetData);
+         btncreate.setOnClickListener(SignUp.this);
+        btnGetAllData=findViewById(R.id.btnGetAllData);
 
-        btncreate.setOnClickListener(SignUp.this);
+        txtGetData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseQuery<ParseObject> parseQuery=ParseQuery.getQuery("kickBoxer");
+                parseQuery.getInBackground("ZXNV1sqnVM", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e)
+                    {
+                        if(object!=null && e==null)
+                        {
+                            txtGetData.setText(object.get("name")+"");
+                        }
 
+
+
+                    }
+                });
+            }
+        });
+
+
+        btnGetAllData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                allKickBoxers="";
+
+               ParseQuery<ParseObject> queryAll=ParseQuery.getQuery("KickBoxer");
+               queryAll.findInBackground(new FindCallback<ParseObject>() {
+                   @Override
+                   public void done(List<ParseObject> objects, ParseException e) {
+                       if(e==null)
+                       {
+                           if(objects.size()>0)
+                           {
+                               for(ParseObject kickBoxer:objects)
+                               {
+                                   allKickBoxers+=kickBoxer.get("name")+"\n";
+                               }
+                               FancyToast.makeText(SignUp.this,   allKickBoxers, FancyToast.LENGTH_LONG, FancyToast.SUCCESS, false).show();
+                           }
+                           else
+                           {
+                               FancyToast.makeText(SignUp.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+                           }
+                       }
+                   }
+               });
+            }
+        });
     }
 
 
